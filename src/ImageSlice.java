fcd8603db0ed937fe188ce3f3a21518eca89385c
rmoeketse <rmoeketse@icloud.com>
2026-04-18 18:46:06 +0200

    Create ImageSlice.java

---
+++ src/ImageSlice.java
@@ -0,0 +1,19 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Thato: 224022442
+ * Refiloe: 221014292
+ * Samrin: 222005020
+ * Naledi: 218104732
+ * @see ImageProcessor.java
+ */
+public class ImageSlice {
+	BufferedImage image;
+	int index;
+	
+	public ImageSlice(BufferedImage image, int index) {
+		this.image = image;
+		this.index = index;
+	}
+	
+}
