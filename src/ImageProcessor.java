89fbcd9003b005a39bee69ca675c564fa170343b
ledi-codes <101285225+ledi-codes@users.noreply.github.com>
2026-04-18 17:51:42 +0200

    Update ImageProcessor.java

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -5,9 +5,24 @@
  * Naledi: 218104732
  * 
  * Handles loading images via ImageIO
+ * Since we are handeling 3D CT scans we will be loading the entire folder of 
+ * images into memory all at once using an array list
  */
+
+//We are going to use these libraries to load the images into java arrays
+import javax.imageio.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
 
 public class ImageProcessor 
 {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+	
+	
+	
 }
